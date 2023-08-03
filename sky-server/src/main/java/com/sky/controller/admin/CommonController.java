8e69bf951c758fe2c310589a70acdd0f08872d98
gpvi <73032307+gpvi@users.noreply.github.com>
2023-08-03 17:13:45 +0800

    新增菜品完成，前后端联调成功

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,43 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.management.ObjectName;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api("通用接口")
+@Slf4j
+public class CommonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file)  {
+        log.info("文件上传");
+        String originFileName = file.getOriginalFilename();
+        String extension = originFileName.substring(originFileName.lastIndexOf("."));
+        String objectName = UUID.randomUUID().toString()+extension;
+        String filePath = null;
+        try {
+            filePath = aliOssUtil.upload(file.getBytes(),objectName);
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+            log.error("文件上传失败L{}"+e);
+        }
+
+        return Result.success(filePath);
+    }
+}
